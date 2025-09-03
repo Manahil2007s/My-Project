40c056a22a5f0a6ec2eb7474b995dc406f8e6247
Manahil Mansoor <manahilmansoor1122@gmail.com>
2025-09-03 21:21:23 +0500

    Update Car rental system.py

--- Car rental system.py
+++ Car rental system.py
@@ -1,3 +1,32 @@
-# Query: 
-# ContextLines: 1
+class CarShop:
+
+    def __init__(self, stock):
+        self.stock = stock
+
+    def displayCars(self):
+        print('Total Cars:', self.stock)
+
+    def sellCars(self, quantity):
+        if quantity <= 0:
+            print("Enter a positive value.")
+        elif quantity > self.stock:
+            print("Not enough cars in stock.")
+        else:
+            self.stock -= quantity
+            print(f"{quantity} car(s) sold.")
+            print("Cars left in stock:", self.stock)
+while True:
+    obj=CarShop(10000)
+    uc=int(input('''
+    1 Display Stocks
+    2 Rent a Car
+    3 Exit
+        '''))
+    if uc==1:
+        obj.displayCars()
+    elif uc==2:
+        n=int(input("Enter the QTY---"))
+        obj.sellCars(n)
+    else:
+        break
 
